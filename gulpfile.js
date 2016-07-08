dfbd8844c171e85cad22ffb397ff6a693492a9aa
Steven Follis <steven_follis@live.com>
2016-07-08 16:05:16 -0400

    Adjusted package

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-var zip = require("gulp-zip");
-var gulp = require('gulp');
-var typescript = require('gulp-tsc');
+const fs = require('fs');
+const zip = require("gulp-zip");
+const gulp = require('gulp');
+const typescript = require('gulp-tsc');
 
 gulp.task("deploy", function () {
   return gulp.src(["**"])
-      .pipe(zip("deploy.zip"))
+      .pipe(zip("deploy/deployment-package.zip"))
       .pipe(gulp.dest("./"));
 });
-
-const fs = require('fs');
 
 gulp.task('compile', function () {
   gulp.src(['**/*.ts', '!**/node_modules/**'])
   .pipe(typescript())
-  .pipe(gulp.dest('./'))
+  .pipe(gulp.dest('./'));
 });
+
+gulp.task('default', ['deploy']);
